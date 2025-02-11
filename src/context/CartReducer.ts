73c56ea216df5d9f6ec26746183a18324f4b8186
danielgbit <danielbitmobb@gmail.com>
2025-02-11 13:03:58 -0500

    Add create Context

---
+++ src/context/CartReducer.ts
@@ -0,0 +1,11 @@
+export const initialState = {
+    cartItems: []
+}
+
+export const cartReducer = (state, action) => {
+    switch(action) {
+        case 'ADD_TO_CART': {
+            const {  } = action.payload
+        }
+    }
+};

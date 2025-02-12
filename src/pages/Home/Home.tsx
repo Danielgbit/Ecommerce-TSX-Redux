47176210d7b74b65011c0fce2fe928620685c99f
danielgbit <danielbitmobb@gmail.com>
2025-02-12 11:55:38 -0500

    Add type TS

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,19 +2,36 @@ import { useEffect, useState } from "react"
 import Hero from "../../components/ui/Hero/Hero"
 import styles from "./Home.module.css";
 import CardProduct from "../../components/ui/CardProduct/CardProduct";
+import { getProducts } from "../../services";
+import { Products } from "../../interface";
 
 
 const Home = () => {
 
-  const [ products, setProducts ] = useState([]);
+  const [ products, setProducts ] = useState<Products[]>([]);
+  const [ error, setError ] = useState(false);
+  const [ loading, setLoading ] = useState(true);
+
 
   useEffect(() => {
-    getProducts();
+    const fetchProducts = async () => {
+      try {
+        const data = await getProducts();
+        setProducts(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchProducts();
   }, []);
 
   return (
     <>
       <Hero/>
+      {loading && <p>LOADING...</p>}
+      {error && <p>Productos no encontrados</p>}
       <div className={styles.container}>
         {products.map((product) => (
           <CardProduct key={product.tail} product={product}/>

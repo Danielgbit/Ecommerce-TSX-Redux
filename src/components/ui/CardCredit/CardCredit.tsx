fd98c727a0c9c7034d9ada416d0eaeb25bf53b33
daniel <danielbitmobb@gmail.com>
2025-02-18 17:42:02 -0500

    CardCredit component in process

---
+++ src/components/ui/CardCredit/CardCredit.tsx
@@ -0,0 +1,25 @@
+import styles from './CardCredit.module.css'
+
+const CardCredit = () => {
+
+
+  return (
+    <div>
+        <div>
+
+        </div>
+
+        <form>
+            <div>
+                <label>Card Number</label>
+                <input type="text" name='number' />
+            </div>
+            <div>
+                <label>Card Name</label>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default CardCredit
